f4d67a5e8a0e8ee178c09e3069bad15c40e7b883
Jackson Emmerich <jacksonemmerich@gmail.com>
2024-11-26 14:21:48 -0400

    first commit

---
+++ src/main/java/com/jacksonemmerich/soft_outlet/SoftOutletApplication.java
@@ -0,0 +1,13 @@
+package com.jacksonemmerich.soft_outlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftOutletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftOutletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacksonemmerich/soft_outlet/SoftOutletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacksonemmerich.soft_outlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftOutletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
